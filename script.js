b914d4fb39a93166a64e867456652ea2eefdea80
RAMYA5577 <121097897+RAMYA5577@users.noreply.github.com>
2023-12-11 21:06:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+var text=document.getElementById(para);
+var button=document.getElementById(btn);
+
+function incrementFun(){
+	
+      var counterValue = document.getElementById('para').innerText;
+
+      var newCounterValue = parseInt(counterValue) + 1;
+
+      // Update the counter value on the webpage
+      document.getElementById('para').innerText = newCounterValue;
+
+      // Display an alert with the un-incremented value
+      alert(counterValue);
+    }
+
